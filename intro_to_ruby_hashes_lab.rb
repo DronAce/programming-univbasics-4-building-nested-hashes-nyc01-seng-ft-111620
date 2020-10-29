4275ee824ed05c5bd8de17d8e145f28820853d66
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-29 16:54:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  hash = monopoly_with_third_tier
+  hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
