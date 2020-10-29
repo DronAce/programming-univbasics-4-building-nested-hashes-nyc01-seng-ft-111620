c5856360d7049046c67530b4bd9796b3f1f829fa
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-29 16:51:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,9 @@ def monopoly_with_third_tier
   # complexity.
   hash = monopoly_with_third_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-  hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 
-  hash[:railroads][:rent_in_dollars][] = 
-  hash[:railroads][:rent_in_dollars][] = 
+  hash[:railroads][:rent_in_dollars][:two_pieces_owned] =
+  hash[:railroads][:rent_in_dollars][] =
+  hash[:railroads][:rent_in_dollars][] =
   hash
 end
 
