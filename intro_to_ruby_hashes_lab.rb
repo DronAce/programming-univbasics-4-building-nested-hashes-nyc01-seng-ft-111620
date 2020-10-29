1d8135615b56dc68c484f105d95e1a2ca243a0d3
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-29 16:48:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,8 @@ def monopoly_with_second_tier
   # complexity.
   hash = base_hash
   hash[:railroads][:pieces] = 4
-  # hash[:railroads][rent_in_dollars:] = {}
-  # hash[:railroads][names:] = {}
+  hash[:railroads][rent_in_dollars:] = {}
+  hash[:railroads][names:] = {}
   hash
 end
 
