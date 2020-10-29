c14109ed0a6cda8d51bd06801456c996fcb97d9c
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-29 16:57:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
   hash = monopoly_with_third_tier
   hash[:railroads][:names][:reading_railroad][:mortgage_value] =
   hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] =
-  hash[:railroads][:names][:reading_railroad][:mortgage_value] =
+  hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] =
   hash[:railroads][:names][:reading_railroad][:mortgage_value] =
   hash
 end
