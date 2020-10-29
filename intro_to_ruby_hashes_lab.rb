e2d878d2cafe1eeb74e39b926046edef9bbe6319
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-29 16:52:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,10 @@ def monopoly_with_third_tier
   hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
   hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  hash[:railroads][:names][:reading_railroad] = {}
+  hash[:railroads][:names][] = {}
+  hash[:railroads][:names][] = {}
+  hash[:railroads][:names][] = {}
   hash
 end
 
